ad1009b9a386961b9af8846c0021ed569c9e6354
satyasaibhushan <satyasaibhushan@gmail.com>
2020-12-06 00:19:59 +0530

    figured out auth

--- server/public/auth.js
+++ server/public/auth.js
@@ -1,30 +1,69 @@
 var auth2; // The Sign-In object.
 var googleUser; // The current user.
+var id_token
+let started = false;
+var verified = false;
 
 var appStart = function() {
-  gapi.load('auth2', initSigninV2); 
+    gapi.load('auth2', initSigninV2); 
 };
 
 var initSigninV2 = function() {
-  auth2 = gapi.auth2.init({
-      client_id: '342980464169-2jqomrchsthgjpdafk50ba8akj22g2v0.apps.googleusercontent.com',
-      scope: 'profile'
-  });
-  auth2.then(()=>{
-      if(auth2.isSignedIn.get()!=true){
-          signIn();
-      }
- })
+    auth2 = gapi.auth2.init({
+        client_id: '342980464169-2jqomrchsthgjpdafk50ba8akj22g2v0.apps.googleusercontent.com',
+        scope: 'profile',
+        // ux_mode: 'redirect',
+    })
+    auth2.then((googleUser)=>{
+        if(auth2.isSignedIn.get()!=true ){
+            signIn();
+        }
+        else{
+            googleUser = auth2.currentUser.ne;
+            id_token = googleUser.getAuthResponse().id_token;
+            verify(id_token);
+        }
+    }).catch(err=>{
+        console.log(err)
+        if(auth2.isSignedIn.get()!=true ){
+            signIn();
+        }
+    })
 };
 
-let signIn =()=>{
-    auth2.signIn().then((googleUser)=>{
-        var profile = googleUser.getBasicProfile();
-        console.log('ID: ' + profile.getId()); // Do not send to your backend! Use an ID token instead.
-        console.log('Name: ' + profile.getName());
-        console.log('Image URL: ' + profile.getImageUrl());
-        console.log('Email: ' + profile.getEmail()); // This is null if the 'email' scope is not present
+let signIn = async()=>{
+    console.log("signing in");
+    auth2.signIn().then(async (googleUser)=>{
+        id_token = googleUser.getAuthResponse().id_token;
+         await verify(id_token);
     })
+}
+
+ let verify=async (id_token)=>{
+     console.log("verifying")
+     await fetch('/tokenVerify', {
+        method: 'POST',
+        mode: 'cors',
+        credentials: 'same-origin', 
+        headers: { 'Content-Type': 'application/json'},
+        redirect: 'follow',
+        body: JSON.stringify({id_token})
+      })
+      .then(res=>res.json())
+      .then(x=>{
+          let {isValid} = x;
+          if(isValid){
+              console.log("You are allowed");
+              verified = true;
+              return true
+          }
+          else{
+            console.log("You are not allowed");
+            // window.location.href = 'http://localhost:5000/error';
+            window.location.replace('http://localhost:5000/error');
+            //   window.redirect('../error');
+          }
+      })
 }
 
 function signOut() {
